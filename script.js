948521ec449304a3e298c6beecd2a1c45db561fb
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2023-10-07 16:51:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let poll = {
+document.addEventListener("DOMContentLoaded", function(){
+  let poll = {
   question: "Minecraft Mob Vote For DevenSMP",
   options:[
     "Crab", "Armadillo", "Penguin"
@@ -8,44 +9,46 @@ let poll = {
   selectedOption:-1
 };
 
-let pollDOM = {
-  question:document.querySelector(".poll .question"),
-  options:document.querySelector(".poll .options")
-};
+  let pollDOM = {
+    question:document.querySelector(".poll .question"),
 
-pollDOM.question.innerText = poll.question;
-poll.options.map(function(answer,i){
-  return (
-    `
+    options:document.querySelector(".poll .options")
+  };
+
+  pollDOM.question.innerText = poll.question;
+  poll.options.map(function(answer,i){
+    return (
+      `
       <div class = "answer" onclick="markAnswer('${i}')">
-        ${answer}
-        <span class = "percentage-bar"></span>
-        <span class = "percentage-value"></span>
+      ${answer}
+      <span class = "percentage-bar"></span>
+      <span class = "percentage-value"></span>
       </div>
-    `
-  );
-}).join("");
+      `
+    );
+  }).join("");
 
-function markAnswer(i){
-  poll.selectedAnswer = +i;
-  try {
-    document.querySelector(".poll .options.answer.selected").classList.remove("selected");
-  } catch(msg){}
-  document.querySelectorAll(".poll .options .answer")[+i].classList.add("selected");
-  showresults();
-}
+  function markAnswer(i){
+    poll.selectedAnswer = +i;
+    try {
+      document.querySelector(".poll .options.answer.selected").classList.remove("selected");
+    } catch(msg){}
+    document.querySelectorAll(".poll .options .answer")[+i].classList.add("selected");
+    showresults();
+  }
 
-function showResults(){
-  let answers = document.querySelectorAll(".poll .options .answer");
-  for(let i=0;i<answers.length;i++){
-    let percentage = 0;
-    if (i == poll.selectedAnswer){
-      percentage = Math.round(
-        (poll.optionsWeight[i]+1) * 100 / (poll.pollCount+1)
-      );
-    }
+  function showResults(){
+    let answers = document.querySelectorAll(".poll .options .answer");
+    for(let i=0;i<answers.length;i++){
+      let percentage = 0;
+      if (i == poll.selectedAnswer){
+        percentage = Math.round(
+          (poll.optionsWeight[i]+1) * 100 / (poll.pollCount+1)
+        );
+      }
+    });
     
     answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
     answers[i].querySelector(".percentage-value").innerText = percentage + "%";
   }
-}
+});
