1c7f868dde0dab7a0e463ede68595c1b87a78d68
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2024-04-24 21:06:17 +0700

    Update redeem.js

--- js/redeem.js
+++ js/redeem.js
@@ -1,5 +1,3 @@
-import "./preventRightClick";
-
 document.getElementById('toggleButton').addEventListener('click', function() {
     const navSide = document.getElementById('navSide');
     navSide.style.display = (navSide.style.display === 'flex') ? 'none' : 'flex';
