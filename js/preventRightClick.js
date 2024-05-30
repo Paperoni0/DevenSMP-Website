87c1a7f285bae2837305ee2277e9e489ee58fce9
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2024-05-30 20:50:26 +0700

    Delete js/preventRightClick.js

--- js/preventRightClick.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-window.oncontextmenu = function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-}
