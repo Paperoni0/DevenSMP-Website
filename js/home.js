b79df858e0ce6c735d9eabe66778ce4dc98c089a
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2024-05-30 20:49:26 +0700

    Delete js/home.js

--- js/home.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', () => {
-    const navSide = document.getElementById('navSide');
-    navSide.style.display = (navSide.style.display === 'flex') ? 'none' : 'flex';
-});
