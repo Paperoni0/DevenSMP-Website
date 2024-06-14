ff0ef1c6e07a0ed6932082f4a50c215a286755ca
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2024-06-14 20:41:58 +0700

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+const countdownDate = new Date("June 15, 2024 10:00:00 UTC").getTime();
+const x = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("days").innerHTML = days + "d";
+    document.getElementById("hours").innerHTML = hours + "h";
+    document.getElementById("minutes").innerHTML = minutes + "m";
+    document.getElementById("seconds").innerHTML = seconds + "s";
+    if (distance < 0) {
+        clearInterval(x);
+    }
+}, 1000);
