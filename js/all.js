04034672adf63ef91ef59941f977adac1d0111c0
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2024-06-15 07:51:09 +0700

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,10 +1,19 @@
-// Prevent Right Click
-document.addEventListener('contextmenu', event => event.preventDefault());
-window.oncontextmenu = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-}
+(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        document.body.classList.add(theme);
+    } else {
+        document.body.classList.add('light');
+    }
+})();
 
-// Menu Button Function
-document.getElementById('menuToggle').addEventListener('click', () => document.getElementById('menu').style.display = (document.getElementById('menu').style.display === 'flex') ? 'none' : 'flex');
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    window.oncontextmenu = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+    document.getElementById('menuToggle').addEventListener('click', () => document.getElementById('menu').style.display = (document.getElementById('menu').style.display === 'flex') ? 'none' : 'flex');
+});
