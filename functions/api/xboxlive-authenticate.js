6a64effc1427e32a6f2d8fdb76bbaf15958016d4
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-03-03 19:41:12 +0700

    Update xboxlive-authenticate.js

--- functions/api/xboxlive-authenticate.js
+++ functions/api/xboxlive-authenticate.js
@@ -11,7 +11,7 @@ export async function onRequest(context) {
   const data = new TextEncoder().encode(timestamp + body);
   const signatureBuffer = await crypto.subtle.sign({ name: "HMAC", hash: "SHA-256" }, await crypto.subtle.importKey("raw", key, { name: "HMAC", hash: "SHA-256" }, false, ["sign"]), data);
   const signature = Array.from(new Uint8Array(signatureBuffer)).map((b) => b.toString(16).padStart(2, "0")).join("");
-  const response = await fetch(`http://${context.env.APIDOMAIN}:${context.env.APIPORT}/xbox-auth`, {
+  const response = await fetch(`http://helya.pylex.xyz:9134/xbox-auth`, {
     method: "POST",
     headers: {
       "Authorization": `HMAC ${signature}`,
