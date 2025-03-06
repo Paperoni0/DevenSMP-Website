e93bf8d02a6d49dd56c68d90ed785ab5da7c9aa6
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-03-06 18:47:15 +0700

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,9 +1,11 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const code = url.searchParams.get("code");
-  const data = url.searchParams.get("state");
-  if (!code || !data) {
+  const state = url.searchParams.get("state");
+  if (!code || !state) {
     return new Response("Error! Something must've gone wrong while attempting to verify you, please contact one of our developers if this problem still persists.");
   }
-  return Response.redirect("https://devensmp.my.to");
+  const decodedState = JSON.parse(atob(state));
+  const data = btoa(JSON.stringify({ code, discord: decodedState.discord, type: decodedState.type }));
+  return Response.redirect(`https://devensmp.my.to?data=${data}`);
 }
