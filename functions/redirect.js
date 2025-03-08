dbc20c101ea82063c03f348a3bb7db04f5852fd5
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-03-08 23:23:20 +0700

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const code = url.searchParams.get("code");
   const state = url.searchParams.get("state");
   if (!code || !state) {
-    return new Response("Error! Something must've gone wrong while attempting to verify you, please contact one of our developers if this problem still persists.");
+    return new Response("Error! Something must've gone wrong while attempting to redirect, please contact one of our developers if this problem still persists.");
   }
   const decodedState = JSON.parse(atob(state));
   const data = btoa(JSON.stringify({ code, discord: decodedState.discord, type: decodedState.type }));
