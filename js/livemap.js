1cdd33df41fde765fff3137f14ab756914e35d0a
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-01 20:02:17 +0700

    Create livemap.js

---
+++ js/livemap.js
@@ -0,0 +1,47 @@
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url: 'tiles/{z}/{x}/{y}.png',
+                tileSize: 256
+            })
+        })
+    ],
+    view: new ol.View({
+        center: [0, 0],
+        zoom: 5
+    })
+});
+
+let playerMarkers = {};
+function updatePlayerLocations() {
+    fetch('/livemapplayers', { headers: { 'Authorization': `Bearer 7d06d670a06774e62b812b94a9e2dde30756d7e0cf608afc90ee7dbaafc63c8c` } })
+        .then(response => response.json())
+        .then(players => {
+            Object.keys(players).forEach(username => {
+                let { x, z, online } = players[username];
+                let coords = [x, -z];
+                if (!playerMarkers[username]) {
+                    playerMarkers[username] = new ol.Feature({
+                        geometry: new ol.geom.Point(coords)
+                    });
+                    playerMarkers[username].setStyle(new ol.style.Style({
+                        image: new ol.style.Icon({
+                            src: online ? 'icons/player_online.png' : 'icons/player_offline.png',
+                            scale: 0.5
+                        })
+                    }));
+                    map.addLayer(new ol.layer.Vector({
+                        source: new ol.source.Vector({
+                            features: [playerMarkers[username]]
+                        })
+                    }));
+                } else {
+                    playerMarkers[username].getGeometry().setCoordinates(coords);
+                }
+            });
+        });
+}
+
+setInterval(updatePlayerLocations, 10000);
