1a5552d15bc6969122566418e7df464c84e3db03
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-01 20:21:43 +0700

    Update livemap.js

--- js/livemap.js
+++ js/livemap.js
@@ -3,16 +3,34 @@ const map = new ol.Map({
     layers: [
         new ol.layer.Tile({
             source: new ol.source.XYZ({
-                url: 'tiles/{z}/{x}/{y}.png',
-                tileSize: 256
+                url: 'assets/images/livemap/tiles/zoom,-{z}/{x}/{y}.jpeg',
+                tileSize: 256,
+                minZoom: -6,
+                maxZoom: 0
             })
         })
     ],
     view: new ol.View({
         center: [-86, 18],
-        zoom: 5
+        zoom: 0
     })
 });
+
+const spawnFeature = new ol.Feature({
+    geometry: new ol.geom.Point([-86, 18])
+});
+spawnFeature.setStyle(new ol.style.Style({
+    image: new ol.style.Icon({
+        src: 'assets/images/livemap/world_spawn.png',
+        scale: 0.5
+    })
+}));
+const spawnLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [spawnFeature]
+    })
+});
+map.addLayer(spawnLayer);
 
 let playerMarkers = {};
 function updatePlayerLocations() {
@@ -28,7 +46,7 @@ function updatePlayerLocations() {
                     });
                     playerMarkers[username].setStyle(new ol.style.Style({
                         image: new ol.style.Icon({
-                            src: online ? 'assets/images/player_online.png' : 'assets/images/player_offline.png',
+                            src: online ? 'assets/images/livemap/player_online.png' : 'assets/images/livemap/player_offline.png',
                             scale: 0.5
                         })
                     }));
