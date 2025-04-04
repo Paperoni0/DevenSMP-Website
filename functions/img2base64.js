cfbbba7990f1046e067a6131efc5a673d6bc96a3
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-04 22:59:49 +0700

    Update img2base64.js

--- functions/img2base64.js
+++ functions/img2base64.js
@@ -1,8 +1,4 @@
 export async function onRequestGet(context) {
-  const apiKey = context.request.headers.get("Authorization");
-  if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) {
-    return new Response("Unauthorized", { status: 401 });
-  }
   const { searchParams } = new URL(context.request.url);
   const imageUrl = searchParams.get("url");
   const response = await fetch(imageUrl);
