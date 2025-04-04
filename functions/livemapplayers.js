98a4b588f8decc1ff389ed973d08c9bdd898350b
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-04 16:08:38 +0700

    Update livemapplayers.js

--- functions/livemapplayers.js
+++ functions/livemapplayers.js
@@ -1,7 +1,7 @@
 export async function onRequestGet(context) {
     const apiKey = context.request.headers.get("Authorization");
     if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) {
-        return new Response(JSON.stringify({ success: false, data: null }), { status: 401 });
+        return new Response("Unauthorized", { status: 401 });
     }
     const storage = context.env.LIVEMAPPLAYERS;
     const keys = await storage.list();
@@ -10,17 +10,17 @@ export async function onRequestGet(context) {
         const data = await storage.get(key.name, { type: "json" });
         players[key.name] = data;
     }
-    return new Response(JSON.stringify({ success: true, data: players }), { headers: { "Content-Type": "application/json" } });
+    return new Response(JSON.stringify(players), { headers: { "Content-Type": "application/json" } });
 }
 
 export async function onRequestPost(context) {
     const apiKey = context.request.headers.get("Authorization");
     if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) {
-        return new Response(JSON.stringify({ success: false, data: "Unauthorized" }), { status: 401 });
+        return new Response("Unauthorized", { status: 401 });
     }
     const data = await context.request.json();
     const { username, x, z, online } = data;
     const storage = context.env.LIVEMAPPLAYERS;
     await storage.put(username, JSON.stringify({ x, z, online }));
-    return new Response(JSON.stringify({ success: true, data: null }), { headers: { "Content-Type": "application/json" } });
+    return new Response("Success");
 }
