fe0f104ca61c43981891fc99ed32df760ac0c899
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-04 16:06:51 +0700

    Create img2base64.js

---
+++ functions/img2base64.js
@@ -0,0 +1,9 @@
+export async function onRequestGet(context) {
+  const { searchParams } = new URL(context.request.url);
+  const imageUrl = searchParams.get("url");
+  const response = await fetch(imageUrl);
+  const buffer = await response.arrayBuffer();
+  const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+  const mimeType = response.headers.get("content-type");
+  return new Response(`data:${mimeType};base64,${base64}`);
+}
