ca7473ccc743e1a2889dd25eaafc0f224264dfcd
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-04 23:15:34 +0700

    Delete functions/img2base64.js

--- functions/img2base64.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  const data = await context.request.json();
-  const imageUrl = data.url;
-  const response = await fetch(imageUrl);
-  const buffer = await response.arrayBuffer();
-  const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)));
-  const mimeType = response.headers.get("content-type");
-  return new Response(`data:${mimeType};base64,${base64}`);
-}
