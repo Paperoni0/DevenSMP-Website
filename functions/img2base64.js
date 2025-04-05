f08b75091ae1d7f27773621e841ed72386905190
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2025-04-05 13:44:59 +0700

    Update img2base64.js

--- functions/img2base64.js
+++ functions/img2base64.js
@@ -1,11 +1,15 @@
 export async function onRequestGet(context) {
-  const apiKey = context.request.headers.get("Authorization");
-  if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) return new Response("Unauthorized", { status: 401 });
-  const { searchParams } = new URL(context.request.url);
-  const imageUrl = searchParams.get("url");
-  const response = await fetch(imageUrl);
-  const buffer = await response.arrayBuffer();
-  const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)));
-  const mimeType = response.headers.get("content-type");
-  return new Respose(`data:${mimeType};base64,${base64}`);
+  try {
+    const apiKey = context.request.headers.get("Authorization");
+    if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) return new Response("Unauthorized", { status: 401 });
+    const { searchParams } = new URL(context.request.url);
+    const imageUrl = searchParams.get("url");
+    const response = await fetch(imageUrl);
+    const buffer = await response.arrayBuffer();
+    const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    const mimeType = response.headers.get("content-type");
+    return new Respose(`data:${mimeType};base64,${base64}`);
+  } catch (error) {
+    return new Response(error, { status: 500 });
+  }
 }
