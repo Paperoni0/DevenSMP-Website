c5af621044dbb9eb1a028098c964a14d4c264a88
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-17 07:09:31 +0700

    Update livemapplayers.js

--- functions/livemapplayers.js
+++ functions/livemapplayers.js
@@ -18,13 +18,15 @@ export async function onRequestPost(context) {
     if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) {
         return new Response("Unauthorized", { status: 401 });
     }
-    const { username, x, z, online } = await context.request.json();
-    const storage = context.env.LIVEMAPPLAYERS;
-    if (x && z) {
-        await storage.put(username, JSON.stringify({ x, z, online }));
-    } else {
-        const { x, z } = await storage.get(username, { type: "json" });
-        await storage.put(username, JSON.stringify({ x, z, online }));
+    const request = await context.request.json();
+    for (const player of request.players) {
+        const storage = context.env.LIVEMAPPLAYERS;
+        if (x && z) {
+            await storage.put(username, JSON.stringify({ x, z, online }));
+        } else {
+            const { x, z } = await storage.get(username, { type: "json" });
+            await storage.put(username, JSON.stringify({ x, z, online }));
+        }
     }
     return new Response("Success");
 }
