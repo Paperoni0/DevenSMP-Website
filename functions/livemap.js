217be200c34b3b68f07834cd1648b971632756ed
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-20 06:35:28 +0800

    Delete functions/livemap.js

--- functions/livemap.js
+++
@@ -1,7 +0,0 @@
-async function onRequestGet(context) {
-  const response = await fetch('http://217.160.125.126:12377/players', { headers: { "Authorization": `Bearer da9b81c4-3dec-4678-be81-f47a3b91d9e0` } });
-  const players = (await response.json()).players;
-  return new Response(JSON.stringify(players), {
-    headers: { "Content-Type": "application/json", "Cache-Control": "no-store" }
-  });
-}
