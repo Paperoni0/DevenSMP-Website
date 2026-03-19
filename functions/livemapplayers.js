293d92ec7c7ea5212383e1390756708a387c7f82
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-20 06:32:13 +0800

    Update livemapplayers.js

--- functions/livemapplayers.js
+++ functions/livemapplayers.js
@@ -1,9 +1,5 @@
 export async function onRequestGet(context) {
-    const apiKey = context.request.headers.get("Authorization");
-    if (!apiKey || apiKey !== `Bearer ${context.env.API_KEY}`) {
-        return new Response("Unauthorized", { status: 401 });
-    }
-    const response = await fetch('http://217.160.125.126:12377/players', { headers: { "Authorization": `Bearer da9b81c4-3dec-4678-be81-f47a3b91d9e0` } });
+    const response = await fetch('http://217.160.125.126:12377/players', { headers: { "Authorization": `Bearer ${context.env.API_KEY}` } });
     const players = (await response.json()).players;
     return new Response(JSON.stringify(players), {
         headers: { "Content-Type": "application/json", "Cache-Control": "no-store" }
