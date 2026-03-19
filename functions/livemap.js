6e8085c58ba01bb6592663ff56565978af42e943
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-19 21:55:28 +0800

    Update livemap.js

--- functions/livemap.js
+++ functions/livemap.js
@@ -1,5 +1,5 @@
 async function onRequestGet(context) {
-  const response = await fetch(context.env.API_URL, { headers: { "Authorization": `Bearer ${context.env.API_KEY}` } });
+  const response = await fetch('http://217.160.125.126:12377/players', { headers: { "Authorization": `Bearer da9b81c4-3dec-4678-be81-f47a3b91d9e0` } });
   const players = (await response.json()).players;
   return new Response(JSON.stringify(players), {
     headers: { "Content-Type": "application/json", "Cache-Control": "no-store" }
