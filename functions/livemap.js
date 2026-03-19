54ac481b47a48a1e91f8b161ec1cb3cc64a1c6fc
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-19 21:53:56 +0800

    Update livemap.js

--- functions/livemap.js
+++ functions/livemap.js
@@ -1,10 +1,7 @@
 async function onRequestGet(context) {
   const response = await fetch(context.env.API_URL, { headers: { "Authorization": `Bearer ${context.env.API_KEY}` } });
   const players = (await response.json()).players;
-  let playerMarkers = [];
-  for (const username of Object.keys(players)) {
-    const { x, z, online, image } = players[username];
-    playerMarkers.push({ name: username, x, z, online, image });
-  }
-  return;
+  return new Response(JSON.stringify(players), {
+    headers: { "Content-Type": "application/json", "Cache-Control": "no-store" }
+  });
 }
