9a594ddfd84ec8a5629153defcaea93d233f63d3
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-03-25 23:00:10 +0700

    Create markers.js

---
+++ js/livemap/overworld/markers.js
@@ -0,0 +1,19 @@
+UnminedCustomMarkers = {
+    isEnabled: true,
+    markers: [
+        {
+            x: -6,
+            z: 23,
+            image: "",
+            imageAnchor: [0.5, 0.5],
+            imageScale: 0.25,
+            text: "World Spawn",
+            textColor: "lime",
+            offsetX: 0,
+            offsetY: 20,
+            font: '12px "MinecraftFont", Arial',
+            textBackgroundColor: "#00000088",
+            textPadding: [2, 4, 2, 4]
+        }
+    ]
+}
